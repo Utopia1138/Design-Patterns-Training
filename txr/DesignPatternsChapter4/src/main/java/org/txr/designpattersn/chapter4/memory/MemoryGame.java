ef2a31645f6b45a286aaf2de25015f16d179bc9e
txr12 <txr12@nowhere.com>
2016-02-16 07:17:49 +0000

    updated

--- txr/DesignPatternsChapter4/src/main/java/org/txr/designpattersn/chapter4/memory/MemoryGame.java
+++ txr/DesignPatternsChapter4/src/main/java/org/txr/designpattersn/chapter4/memory/MemoryGame.java
@@ -1,47 +1,44 @@
 package org.txr.designpattersn.chapter4.memory;
 
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.concurrent.ThreadLocalRandom;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import com.jme3.scene.control.*;
+import com.jme3.system.AppSettings;
 
 public class MemoryGame extends SimpleApplication {
-
+	private static int NUMBER_OF_SHAPES = 5;
+	protected Node shapeNode;
 	@Override
 	public void simpleInitApp() {
-
-		ShapeCreator creator1 = getRandomShapeCreator();
-		ShapeCreator creator2 = getRandomShapeCreator();
-		ShapeCreator creator3 = getRandomShapeCreator();
 		
-		
-		Geometry geom1 = creator1.createShape(getRandomColor().getColor(), assetManager);
-		Geometry geom2 = creator2.createShape(getRandomColor().getColor(), assetManager);
-		geom2.setLocalTranslation(new Vector3f(3f, 0f, 0f));
-		Geometry geom3 = creator3.createShape(getRandomColor().getColor(), assetManager);
-		geom3.setLocalTranslation(new Vector3f(6f, 0f, 0f));
-
-		Node shapeNode = new Node();
-		shapeNode.attachChild(geom1);
-		shapeNode.attachChild(geom2);
-		shapeNode.attachChild(geom3);
+		flyCam.setEnabled(false);
+		shapeNode = new Node();
+		float xStart = -7f;
+		for (int i =0; i < NUMBER_OF_SHAPES; i++) {
+			ShapeCreator creator = getRandomShapeCreator();
+			Geometry geom = creator.createShape(getRandomColor().getColor(), assetManager);
+			geom.setLocalTranslation(new Vector3f(xStart, 0f, 0f));
+			xStart +=4;
+			shapeNode.attachChild(geom);
+			
+		}
+		shapeNode.setLocalScale(0.5f);
+		shapeNode.addControl(new MyControl());
 		rootNode.attachChild(shapeNode);
+	
 	}
 
 	public static void main(String[] args) {
+		AppSettings settings = new AppSettings(true);
+		settings.setTitle("Memory Game");
 		MemoryGame app = new MemoryGame();
+		app.setSettings(settings);
 		app.start();
-	}
-
-	private List<Geometry> createGeometries(int number) {
-		List<Geometry> list = new ArrayList<Geometry>();
-
-		return list;
 	}
 
 	private Color getRandomColor() {
@@ -66,6 +63,23 @@ public class MemoryGame extends SimpleApplication {
 		
 
 	}
-
+	
+	class MyControl extends UpdateControl{
+		float time = 3f;
+		float timeCount;
+		public MyControl() {
+			
+		}
+		protected void controlUpdate(float tpf) {
+			timeCount = timeCount + tpf;
+			if (timeCount > time) {
+				spatial.getParent().detachChild(spatial);
+			}
+		}
+		
+	}
+	
+	
+	
 
 }
